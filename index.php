9847625c8a9a545b7f84adf8eb20cc35f54da087
Joel-Irineu <irineu.joel01@gmail.com>
2022-08-05 14:06:15 -0300

    Deletar dados do banco

--- index.php
+++ index.php
@@ -7,6 +7,13 @@
         $sql->execute(array($_POST['name'],$_POST['email']));
         echo '<script>alert("inserido com sucesso!")</script>';
     }
+    // Delete
+    if(isset($_GET['delete'])){
+        $id = (int)$_GET['delete'];
+
+        $pdo->exec("DELETE FROM clients WHERE id=$id");
+        echo '<script>alert("deletado com sucesso!")</script>'; 
+    }
 ?>
 
 <form action="" method="post"> 
@@ -22,7 +29,7 @@
     $fetchClients = $sql->fetchAll();
 
     foreach($fetchClients as $key => $value){
-        echo $value['name'].' | '.$value['email'];
+        echo $value['name'].' | '.$value['email'].' <a href="?delete='.$value['id'].'">excluir</a>';
         echo '<hr>';
     }
 ?>
